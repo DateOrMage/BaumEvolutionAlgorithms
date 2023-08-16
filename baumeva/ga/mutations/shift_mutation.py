efb69d0a4209e799c908941618364935020cf4c2
Daniel Ageev <daniel.ageev0000@mail.ru>
2023-08-16 15:46:43 +0300

    Update shift_mutation.py

--- baumeva/ga/mutations/shift_mutation.py
+++ baumeva/ga/mutations/shift_mutation.py
@@ -5,7 +5,7 @@ from .base_combinatory_mutation import BaseCombinatoryMutation
 class ShiftMutation(BaseCombinatoryMutation):
 
     def get_mutation(self, child: dict) -> dict:
-        chain_length = randint(1, len(child['genotype']) - 1)
+        chain_length = randint(1, len(child['genotype']) - 2)
         s_idx = randint(0, len(child['genotype']) - chain_length)
         e_idx = s_idx + chain_length
 
