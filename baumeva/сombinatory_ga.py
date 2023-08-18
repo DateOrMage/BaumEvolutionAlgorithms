22527418f6a43bb16c1d7a3796aae43aaa3a22f3
Aleksei Kudriavtsev <vatutu@gmail.com>
2023-08-18 14:59:41 +0300

    refactoring combinatory ga doctrings

--- baumeva/сombinatory_ga.py
+++ baumeva/сombinatory_ga.py
@@ -4,11 +4,39 @@ from .ga import GaData, OrderCatPopulation, HyperbolaFitness, PenaltyFunction, T
 
 
 class CombinatoryGA:
+    """
+    Class for perform combinatory genetic algorithm (categorical order combinations without repetitions).
+    """
     def __init__(self, num_generations: int, num_individ: int, gens: tuple, obj_function: Callable,
                  obj_value: Union[int, float] = None, input_data: Any = None, penalty: PenaltyFunction = None,
                  children_percent: float = 0.9, early_stop: int = 10, input_population: List[list] = None,
-                 tournament_size: int = 3, mutation_lvl: Union[str, float] = 'normal', transfer_parents: str = 'best')\
-                 -> None:
+                 tournament_size: int = 3, mutation_lvl: Union[str, float] = 'normal',
+                 transfer_parents: str = 'best') -> None:
+        """
+        Initialization CombinatoryGA with next parameters:
+        :param num_generations: int, number of generations;
+        :param num_individ: int, number of individuals in generation (size of population);
+        :param gens: tuple, tuple of 3 integer value, example: (0, 9, 10), 0 - first categorical value,
+                     9 - last categorical value, step between categorical is 1 (const), 10 - number of categorical
+                     values in every individ, u can use value in range 9-0+1 = 10;
+        :param obj_function: Callable, object function with 1 or 2 arguments, my_func(gens: list) or
+                             my_func(input_data: Any, gens: list);
+        :param obj_value: int | float, default: None. If object value exists, GA will optimize to the value,
+                          else GA will optimize to min;
+        :param input_data: Any, default: None. Argument for object function;
+        :param penalty: PenaltyFunction, default: None. Subclass of PenaltyFunction(), initialization before
+                        initialization class CombinatoryGA(), used for conditional optimization.
+                        Example: Dynamic([(my_conditional_func_1, 'inequal'), (my_conditional_func_2, 'equal)]);
+        :param children_percent: float, default: 0.9. Percent of children who will be in new generation;
+        :param early_stop: int, default: 10. Early stopping criteria, number of generation without improve;
+        :param input_population: list[list], default: None. First generation from user;
+        :param tournament_size: int, default: 3. Size of tournament, use only with selection_type="tournament";
+        :param mutation_lvl: str | float, default: 'normal'. Mutations gens with different parameters:
+                             float value: 0.00,...,1.00;
+                             str value: 'weak', 'normal', 'strong';
+        :param transfer_parents: str, default: "best". Type of transfer parents: "best", "random"
+        :return None
+        """
         self.num_generations = num_generations
         self.num_individ = num_individ
         self.gens = gens
@@ -24,23 +52,28 @@ class CombinatoryGA:
         self.transfer_parents = transfer_parents
 
     def optimize(self) -> GaData:
+        """
+        Main method of CombinatoryGA().
+        :return: GaData
+        """
+        # init GaData & Population
         ga_data = GaData(num_generations=self.num_generations, children_percent=self.children_percent,
                          early_stop=self.early_stop)
         population = OrderCatPopulation()
         population.set_params(num_individ=self.num_individ, gens=self.gens, input_population=self.input_population)
-
+        # init fitness func, selection, crossover, mutation, new generation
         fitness_func = HyperbolaFitness(obj_function=self.obj_function, obj_value=self.obj_value,
                                         input_data=self.input_data, penalty=self.penalty)
         selection = TournamentSelection(tournament_size=self.tournament_size)
         cross = OrderCrossover()
         mutation = InversionMutation(mutation_lvl=self.mutation_lvl)
         new_generation = NewGeneration(transfer_parents=self.transfer_parents)
-
+        # creating first generation
         population.fill()
         ga_data.population = population
         fitness_func.execute(data=ga_data)
         ga_data.update()
-
+        # main loop for GA perform
         for i in range(ga_data.num_generations):
 
             selection.execute(ga_data)
