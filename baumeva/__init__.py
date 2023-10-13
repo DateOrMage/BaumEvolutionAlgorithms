af67423a6dbae8f485da06a7850a57bdbaf01aee
Aleksei Kudriavtsev <vatutu@gmail.com>
2023-10-13 15:02:01 +0300

    delete some legacy

--- baumeva/__init__.py
+++ baumeva/__init__.py
@@ -12,17 +12,6 @@ __version__ = '0.4.4'
 # from .ga import BaseCrossover, OrderCrossover, OnePointCrossover, TwoPointCrossover, UniformCrossover
 # from .ga import BaseMutation, BaseCombinatoryMutation, InversionMutation, SwapMutation, MovementMutation,\
 #                 ShiftMutation, BinStringMutation
-# from .combinatory_ga import CombinatoryGA
-# from .binary_ga import BinaryGA
 
-# from .ga import BasePenalty, DynamicPenalty
-# from .ga import GaData
-# from .ga import NewGeneration
-# from .ga import BasePopulation, CatPopulation, OrderCatPopulation, BinaryPopulation, BinaryGrayPopulation
-# from .ga import BaseFitness, HyperbolaFitness
-# from .ga import BaseSelection, TournamentSelection, BalancedSelection, RankedSelection
-# from .ga import BaseCrossover, OrderCrossover, OnePointCrossover, TwoPointCrossover, UniformCrossover
-# from .ga import BaseMutation, BaseCombinatoryMutation, InversionMutation, SwapMutation, MovementMutation,\
-#                 ShiftMutation, BinStringMutation
 from .combinatory_ga import CombinatoryGA
 from .binary_ga import BinaryGA
