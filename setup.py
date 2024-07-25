a16c60245839316abef98418089d051cbfee8067
Aleksei Kudriavtsev <vatutu@gmail.com>
2024-07-25 12:12:14 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
                      "Programming Language :: Python :: 3.9",
                      "Programming Language :: Python :: 3.10",
                      "Programming Language :: Python :: 3.11",
+                     "Programming Language :: Python :: 3.12",
                      "License :: OSI Approved :: MIT License",
                      "Operating System :: OS Independent"]
     )
